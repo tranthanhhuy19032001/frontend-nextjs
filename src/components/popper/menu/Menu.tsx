1a5a19f3f4dfd0e94a0135b178aa2065e7bddfb5
ThanhHuy <tranthanhhuy19032001.ltp@gmail.com>
2024-03-02 21:14:43 +0700

    [FE] Implement UI popper account menu

--- src/components/popper/menu/Menu.tsx
+++ src/components/popper/menu/Menu.tsx
@@ -1,66 +1,73 @@
-"use client";
+'use client'
 
-import Tippy from "@tippyjs/react/headless";
-import { useState } from "react";
-import PopperWrapper from "../PopperWrapper";
-import MenuItem from "./MenuItem";
-import Header from "./Header";
+import Tippy from '@tippyjs/react/headless'
+import { useState } from 'react'
+import PopperWrapper from '../PopperWrapper'
+import MenuItem from './MenuItem'
+import Header from './Header'
 
-const defaultFn = () => {};
+const defaultFn = () => {}
 
 export default function Menu({
-  children,
-  items = [],
-  onChange = defaultFn,
+    children,
+    items = [],
+    onChange = defaultFn,
 }: any) {
-  const [history, setHistory] = useState([{ data: items }]);
-  const current = history[history.length - 1];
+    const [history, setHistory] = useState([{ data: items }])
+    const current = history[history.length - 1]
 
-  const renderItems = () => {
-    return current.data.map((item: any, index: any) => {
-      const isParent = !!item.children;
+    const renderItems = () => {
+        return current.data.map((item: any, index: any) => {
+            const isParent = !!item.children
 
-      return (
-        <MenuItem
-          key={index}
-          data={item}
-          onClick={() => {
-            if (isParent) {
-              setHistory((prev) => [...prev, item.children]);
-            } else {
-              onChange(item);
-            }
-          }}
-        />
-      );
-    });
-  };
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children])
+                        } else {
+                            onChange(item)
+                        }
+                    }}
+                />
+            )
+        })
+    }
 
-  return (
-    <Tippy
-      interactive
-      visible={true}
-      delay={[0, 600]}
-      offset={[12, 8]}
-      placement="bottom-end"
-      render={(attrs) => (
-        <div className="" tabIndex={-1} {...attrs}>
-          <PopperWrapper className="">
-            {history.length > 1 && (
-              <Header
-                title="Language"
-                onBack={() => {
-                  setHistory((prev) => prev.slice(0, prev.length - 1));
-                }}
-              />
+    return (
+        <Tippy
+            arrow={true}
+            interactive
+            visible={true}
+            delay={[0, 600]}
+            offset={[12, 8]}
+            placement="bottom-end"
+            render={(attrs) => (
+                <div
+                    className="border-2 rounded mt-3 shadow-md z-10"
+                    tabIndex={-1}
+                    {...attrs}
+                >
+                    <PopperWrapper className="">
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory((prev) =>
+                                        prev.slice(0, prev.length - 1),
+                                    )
+                                }}
+                            />
+                        )}
+                        {renderItems()}
+                    </PopperWrapper>
+                </div>
             )}
-            {renderItems()}
-          </PopperWrapper>
-        </div>
-      )}
-      onHide={() => setHistory((prev) => prev.slice(0, 1))}
-    >
-      {children}
-    </Tippy>
-  );
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+            {children}
+        </Tippy>
+    )
 }
--- src/components/popper/menu/MenuItem.tsx
+++ src/components/popper/menu/MenuItem.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import Button from '@/components/button/Button'
+import React from 'react'
 
 interface MenuItemProps {
-  data: {
-    icon: React.ReactNode; // Assuming your icon is a React Node
-    to: string;
-    title: string;
-    separate?: boolean;
-  };
-  onClick: () => void;
+    data: {
+        icon: React.ReactNode // Assuming your icon is a React Node
+        to: string
+        title: string
+        separate?: boolean
+    }
+    onClick: () => void
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ data, onClick }) => {
-  return (
-    <button className="block" onClick={onClick}>
-      {data.title}
-    </button>
-  );
-};
+    return (
+        <Button
+            href={data.to}
+            className="block p-2 hover:bg-gray-200"
+            leftIcon={data.icon}
+        >
+            {data.title}
+        </Button>
+    )
+}
 
-export default MenuItem;
+export default MenuItem
