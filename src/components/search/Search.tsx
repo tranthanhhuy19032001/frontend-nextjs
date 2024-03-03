0b77e1ada93eadbb6da80d552b7b5c40cce1180d
ThanhHuy <tranthanhhuy19032001.ltp@gmail.com>
2024-03-03 20:32:09 +0700

    [FE] [Search UI] fix responsive

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -17,29 +17,29 @@ const Search = () => {
   };
 
   return (
-    <div className="relative flex items-center gap-1 max-[1024px]:hidden">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="px-2 py-1 border border-gray-300 rounded"
-      />
-      {searchQuery && (
-        <FontAwesomeIcon
-          icon={faTimes}
-          className="absolute inset-y-2 right-12 text-gray-400 pr-1"
-          onClick={handleClear}
-        />
-      )}
-      <button
-        onClick={handleSearch}
-        className="px-3 py-1 bg-blue-500 text-white rounded"
-      >
-        <FontAwesomeIcon icon={faSearch} />
-      </button>
-    </div>
-  );
+      <div className="relative flex items-center gap-1 max-[1130px]:hidden">
+          <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="px-2 py-1 border border-gray-300 rounded"
+          />
+          {searchQuery && (
+              <FontAwesomeIcon
+                  icon={faTimes}
+                  className="absolute inset-y-2 right-12 text-gray-400 pr-1"
+                  onClick={handleClear}
+              />
+          )}
+          <button
+              onClick={handleSearch}
+              className="px-3 py-1 bg-blue-500 text-white rounded"
+          >
+              <FontAwesomeIcon icon={faSearch} />
+          </button>
+      </div>
+  )
 };
 
 export default Search;
